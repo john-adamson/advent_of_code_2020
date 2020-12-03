7a3c3fbb3857f55cabf119e6b68579c2da444354
Oxford Dynamics <john@oxforddynamics.com>
2020-12-03 11:55:27 +0000

    Adding day 2 and 3

--- 01/part1.py
+++
@@ -1,5 +0,0 @@
-from common import audit
-
-if __name__ == '__main__':
-    product = audit(r_length=2)
-    print(product if product else 'No solution found')
--- 01/part2.py
+++
@@ -1,5 +0,0 @@
-from common import audit
-
-if __name__ == '__main__':
-    product = audit(r_length=3)
-    print(product if product else 'No solution found')
